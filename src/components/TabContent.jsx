0ff764d3576de32a735f92f52a950303f6125af3
Tu Nombre <tuemail@dominio.com>
2024-10-29 23:07:41 -0500

    margenimg

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -8,7 +8,7 @@ const TabContent = ({ activeTab }) => {
     const containerStyles = "rounded-3xl border border-black pt-6 mb-4 min-h-[60vh]";
   const contentLeftColumn = "text-center md:text-left md:w-1/2 space-y-6"; 
   const imageLeftColumn = "hidden md:flex md:w-1/2 justify-start ml-28";
-  const imageRightColumn = "hidden md:flex md:w-1/2 justify-end mr-10";
+  const imageRightColumn = "hidden md:flex md:w-1/2 justify-end mr-32";
   const image2RightColumn = "hidden md:flex md:w-1/2 justify-end mr-0 rounded-xl";
 
   if (activeTab === 'CDTs') {
