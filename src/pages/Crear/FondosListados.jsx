48923066cdafdd00098cd9057f7b2b9c34b52f5d
Tu Nombre <tuemail@dominio.com>
2025-01-09 20:02:09 -0500

    .

--- src/pages/Crear/FondosListados.jsx
+++ src/pages/Crear/FondosListados.jsx
@@ -1,15 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import skandia from '../../assets/images/skandia.png';
 import Lista from '../../components/Lista';
 
 function FondosListados() {
     const titulos = ['Nombre portafolio', 'Tipo de inversión', 'Perfil de riesgo', 'Pago mínimo de entrada', 'Valor de fondo'];
-    const datos = [
-        ['FPV acciones global', 'RV', 'Alto', '1 MM COP', '50 000MM COP'],
-        ['FPV acciones tecnología', 'RV', 'Alto', '1 MM COP', '50 000MM COP'],
-        ['FPV Skandia', 'RV', 'Alto', '1 MM COP', '50 000MM COP'],
-        ['FPV strategist conservador', 'RV', 'Alto', '1 MM COP', '50 000MM COP']
-    ];
+    const [datos, setDatos] = useState([]); // Estado para los datos
     const colores = ['#0015CD', 'black', 'black', 'black', 'black'];
+
+    useEffect(() => {
+
+        fetch('http://mouback.endomedicos.com/fondos-skandia')
+            .then((response) => response.json())
+            .then((data) => {
+                const datosFormateados = data.map((fondo) => [
+                    fondo.nombre_portafolio,
+                    fondo.tipo_inversion,
+                    fondo.perfil_riesgo,
+                    `${fondo.pago_minimo_entrada.toLocaleString()} COP`,
+                    `${fondo.valor_fondo.toLocaleString()} COP`
+                ]);
+                setDatos(datosFormateados);
+            })
+            .catch((error) => console.error('Error al cargar los fondos:', error));
+    }, []);
 
     return (
         <div className="mx-auto px-4 sm:px-6 lg:px-72 bg-white font-sans">
@@ -18,7 +31,11 @@ function FondosListados() {
                 <img src={skandia} alt="Skandia Logo" className="h-16" />
             </div>
             <div className="w-full mt-24 mb-40">
-                <Lista titles={titulos} data={datos} colors={colores} />
+                {datos.length > 0 ? (
+                    <Lista titles={titulos} data={datos} colors={colores} />
+                ) : (
+                    <p className="text-center text-gray-500">Cargando fondos de inversión...</p>
+                )}
             </div>
         </div>
     );
